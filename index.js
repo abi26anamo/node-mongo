b350f9dd6da8b5ac8279d566eca97a8649ca296e
abi26anamo <abianamo282@gmail.com>
2022-09-04 13:43:31 -0700

    updated some files

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const mongoClient =require('mongodb').MongoClient;
 const assert = require('assert');
+const dboper = require('./operations');
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
 
 mongoClient.connect(url,(err,client)=>{
     assert.equal(err,null);
     console.log('Connected correctly to the database');
+    dboper.insertDocument(db,{name:"vadonut",description:"Test"},'dishes',(result)=>{
+        console.log('Insert Document :n',result.ops);
+        dboper.findDocuments(db,'dishes',(docs)=>{
+            console.log('Found documents:\n',docs);
+
+            dboper.updateDocument(db,{name:"vadonut"},{description:"Updated test"},'dishes',(result)=>{
+                console.log('Updated document:\n',result.result);
+                dboper.findDocuments(db,'dishes',(docs)=>{
+                    console.log('Found documents:\n',docs);
+
+                    db.dropCollection('dishes',(result)=>{
+                        console.log('Dropped Collection',result);
+                    });
+        });
+            });
+        });
+    });
     const db = client.db(dbname);
     const collection= db.collection('dishes');
     collection.insertOne({"name":"abinet","description":"test"},(err,result)=>{
